0af5e60844d48927b62976939ff474850b3ab6ba
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-02-10 17:02:39 -0500

    0210-9

--- components/map.js
+++ components/map.js
@@ -14,20 +14,20 @@ function map() {
       </div>
       <div class='mt-4 px-6 text-sm text-slate-400'>
             <ul className='font-bold'>
-                <li>ANNA BEAUTY : 832 Spur 138 Jonesboro GA 30236</li>
-                <li >BEAUTY 7 MART : 632 Thornton Rd Lithia Springs GA 30122</li>
-                <li>BEAUTY FOR LESS : 3277 Buford HWY #625 Brookheaven GA 30329</li>
-                <li>BEAUTY NOW :  4450 Nelson brogdon Blvd Buford GA 30518</li>
-                <li>BEAUTY TOWN HUDSON : 1763 Hudson Bridge RD Stockbridge GA 30281</li>
-                <li>FANTASTIC BEAUTY SUPPLY : 3615 Flakes Mill RD. Decatur GA 30034</li>
-                <li>MIN'S BEAUTY : 4919 Flat Shoals Pkwy #104 Decatur GA 30034</li>
-                <li>FEEL THE BEAUTY : 1772 Jonesboro Road suite 200 B McDonough GA 30253</li>
-                <li>HAIR CROWN & WIG : 3441 memorial Dr Decatur GA 30032</li>
-                <li>LUX BEAUTY SUPPLY : 4044 Atlanta Hwy suite #816 Loganville GA 30052</li>
-                <li>JC BEAUTY : 4847 Jonesboro Rd. Forest Park GA 30297</li>
-                <li>SO GOOD BRIDAL & BEAUTY : 3480 Steve Reynolds Blvd. Duluth GA 30096</li>
-                <li>SUNNY BEAUTY SUPPLY : 1426 Dogwood Dr. Conyers GA 30013</li>
-                <li>THE FASHION : 93 Temple Ave Newnan GA 30263</li>
+                <li>ANNA BEAUTY - 832 Spur 138 Jonesboro GA 30236</li>
+                <li >BEAUTY 7 MART - 632 Thornton Rd Lithia Springs GA 30122</li>
+                <li>BEAUTY FOR LESS - 3277 Buford HWY 625 Brookheaven GA 30329</li>
+                <li>BEAUTY NOW -  4450 Nelson brogdon Blvd Buford GA 30518</li>
+                <li>BEAUTY TOWN HUDSON - 1763 Hudson Bridge RD Stockbridge GA 30281</li>
+                <li>FANTASTIC BEAUTY SUPPLY - 3615 Flakes Mill RD. Decatur GA 30034</li>
+                <li>MINS BEAUTY - 4919 Flat Shoals Pkwy 104 Decatur GA 30034</li>
+                <li>FEEL THE BEAUTY - 1772 Jonesboro Road suite 200 B McDonough GA 30253</li>
+                <li>HAIR CROWN & WIG - 3441 memorial Dr Decatur GA 30032</li>
+                <li>LUX BEAUTY SUPPLY - 4044 Atlanta Hwy suite 816 Loganville GA 30052</li>
+                <li>JC BEAUTY - 4847 Jonesboro Rd. Forest Park GA 30297</li>
+                <li>SO GOOD BRIDAL & BEAUTY - 3480 Steve Reynolds Blvd. Duluth GA 30096</li>
+                <li>SUNNY BEAUTY SUPPLY - 1426 Dogwood Dr. Conyers GA 30013</li>
+                <li>THE FASHION - 93 Temple Ave Newnan GA 30263</li>
             </ul>
         </div>
     </div>
