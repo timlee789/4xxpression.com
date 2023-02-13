6f2d83e7539ef3c43aa74c617e80f5274dd4946f
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-02-13 15:40:49 -0500

    0213-5

--- components/map.js
+++ components/map.js
@@ -4,7 +4,7 @@ import Iframe from 'react-iframe';
 function map() {
   return (
     <div class='mt-12 mb-12 '>
-        <div class='text-xl mb-2 font-bold px-4 py-2 text-blue-600'>The Stores which is selling the 4X Xpression</div>
+        <div class='text-sm mb-2 font-bold px-4 py-2 text-blue-600'>The Stores which are selling 4X Xpression, Brazilian Trio Bundle, Soprano Clip Hair</div>
         
       <div className='border-4 border-slate-300'>
         {/* <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13218.051208748759!2d-83.94266884999999!3d34.082001399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1675274122189!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/> */}
--- components/regulation.js
+++ components/regulation.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function regulation() {
   return (
-    <div className='bg-slate-100'>
+    <div className=''>
         <div className='text-2xl mb-2 font-bold px-4 text-blue-600'>Contest Regulation</div>
-        <ul className='px-6 text-xs text-slate-500 py-2'>
+        <ul className='px-6 text-xs text-slate-500 py-2 bg-slate-100'>
             <li> 1. You may participate in all three categories as long as you submit different styles for each. </li>
             <li> 2. Please only use photos of your own personal work. </li>
             <li> 3. Must submit any and all submissions by March 31, 2023.  </li>
@@ -15,6 +15,17 @@ function regulation() {
             <li> 8. Winners will be announced on April 5, 2023 on Instagram and Facebook. </li>
             <li> 9. If you tag your Instagram, other photos of your work may be considered when selecting a winner. </li>
         </ul>
+        <div className='text-md mt-4 mb-2 font-bold px-4 text-blue-600'>Photo Copyright Release</div>
+        <div className='px-6 text-xs text-slate-500 py-2 bg-slate-100'>
+        I here grant Beauty Elements, Inc their assigns, Licensees and legal representatives 
+        the irrevocable right to copyright, publish and use in any form of media, 
+        for advertising, trade, stock use or other lawful purpose any likeness or photograph. 
+        I waive the right to inspect or approve the finished product, including the written copy. 
+        I hereby release and agree to hold harmless Beauty Elements, Inc. their assigns and those operating 
+        under their authority from any liability by virtue or the lawful use of these pictures. 
+        By submit above contents, I acknowledge the established obligations and stipulations stated above 
+        and agree to these terms, which are binding.
+        </div>
     </div>
   )
 }
