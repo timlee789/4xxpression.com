65d351403c49454293f18c699dd064b1d7f120d7
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-02-14 11:15:07 -0500

    0214-1

--- components/eventHead.js
+++ components/eventHead.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
+import ReactPlayer from 'react-player';
 
 function eventHead() {
   return (
     <div>
       <div>
          <Image src={'/images/banner-top.jpg'} alt='head' width={1050} height={850} />
+      </div>
+      <div className='text-2xl text-center font-grey-600 font-bold'>Rachel is Introducing the Event </div>
+      <div className='bg-slate-200 mb-4 py-4'>
+        
+          <ReactPlayer url='/images/haireventintro.mp4' playing='true' width={600}/>
+       
+       
       </div>
       <div>
         <Image src={'/images/prizebraid2.jpg'} alt='head' width={600} height={300} className='mb-4' />
