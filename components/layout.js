112c3e02472d75bcb65e6a2bdedb3e9071dac1d9
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-03-08 13:20:14 -0500

    0308-2

--- components/layout.js
+++ components/layout.js
@@ -13,17 +13,17 @@ function Layout({ title, children }) {
     <div>
       <Head>
         <title>{title ? title + '-Bijoux' : '4X Xpression'}</title>
-        <meta name="description" content=" Realistic Xpression Hair Style Photo Contest" />
+        <meta name="description" content="Hair Wholesaler-Beauty Elements-is hosting the contest" />
         <link
           rel="image_src"
           href="/images/pavicon.jpg"
         />
-        <meta property="og:title" content="Realistic Hair Style Photo Contest" />
+        <meta property="og:title" content="Hair Style Photo Contest" />
         <meta
           property="og:image"
           content="/images/pavicon.jpg"
         />
-        <meta property="og:description" content="Realistic Hair Style Photo Contest" />
+        <meta property="og:description" content= "Hair Wholesaler-Beauty Elements-is hosting the contest" />
         <meta property="og:site_name" content="Beauty Elements" />
       </Head>
       <ToastContainer position="bottom-center" limit={1} autoClose={2000} />
